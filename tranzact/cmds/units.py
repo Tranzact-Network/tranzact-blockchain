feba6579c41a2b625020b363b5eec9536c567ae9
archiviz <archiviz101@gmail.com>
2021-09-03 00:23:17 -0500

    renamed mojo to zacto removed premine

--- tranzact/cmds/units.py
+++ tranzact/cmds/units.py
@@ -1,9 +1,9 @@
 from typing import Dict
 
-# The rest of the codebase uses mojos everywhere.
+# The rest of the codebase uses zactos everywhere.
 # Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "tranzact": 10 ** 12,  # 1 tranzact (TRZ) is 1,000,000,000,000 mojo (1 trillion)
-    "mojo:": 1,
-    "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin mojos
+    "tranzact": 10 ** 12,  # 1 tranzact (TRZ) is 1,000,000,000,000 zacto (1 trillion)
+    "zacto:": 1,
+    "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin zactos
 }
--- tranzact/consensus/block_rewards.py
+++ tranzact/consensus/block_rewards.py
@@ -1,7 +1,7 @@
 from tranzact.util.ints import uint32, uint64
 
-# 1 Tranzact coin = 1,000,000,000,000 = 1 trillion mojo.
-_mojo_per_tranzact = 1000000000000
+# 1 Tranzact coin = 1,000,000,000,000 = 1 trillion zacto.
+_zacto_per_tranzact = 1000000000000
 _blocks_per_year = 1681920  # 32 * 6 * 24 * 365
 
 
@@ -13,19 +13,19 @@ def calculate_pool_reward(height: uint32) -> uint64:
     (3 years, etc), due to fluctuations in difficulty. They will likely come early, if the network space and VDF
     rates increase continuously.
     """
-
-    if height == 0:
-        return uint64(int((7 / 8) * 600000 * _mojo_per_tranzact))
-    elif height < 2 * _blocks_per_year:
-        return uint64(int((7 / 8) * 5 * _mojo_per_tranzact))
-    elif height < 4 * _blocks_per_year:
-        return uint64(int((7 / 8) * 3 * _mojo_per_tranzact))
-    elif height < 7 * _blocks_per_year:
-        return uint64(int((7 / 8) * 1.5 * _mojo_per_tranzact))
+    #no premine, but early adopters get paid!
+    if height < _blocks_per_year / 12:
+        return uint64(int((7 / 8) * 15 * _zacto_per_tranzact))
+    elif height < 1 * _blocks_per_year:
+        return uint64(int((7 / 8) * 5 * _zacto_per_tranzact))
+    elif height < 3 * _blocks_per_year:
+        return uint64(int((7 / 8) * 2 * _zacto_per_tranzact))
+    elif height < 6 * _blocks_per_year:
+        return uint64(int((7 / 8) * 1 * _zacto_per_tranzact))
     elif height < 12 * _blocks_per_year:
-        return uint64(int((7 / 8) * 0.75 * _mojo_per_tranzact))
+        return uint64(int((7 / 8) * 0.5 * _zacto_per_tranzact))
     else:
-        return uint64(int((7 / 8) * 0.25 * _mojo_per_tranzact))
+        return uint64(int((7 / 8) * 0.25 * _zacto_per_tranzact))
 
 
 def calculate_base_farmer_reward(height: uint32) -> uint64:
@@ -37,15 +37,16 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
     (3 years, etc), due to fluctuations in difficulty. They will likely come early, if the network space and VDF
     rates increase continuously.
     """
-    if height == 0:
-        return uint64(int((1 / 8) * 600000 * _mojo_per_tranzact))
-    elif height < 2 * _blocks_per_year:
-        return uint64(int((1 / 8) * 5 * _mojo_per_tranzact))
-    elif height < 4 * _blocks_per_year:
-        return uint64(int((1 / 8) * 3 * _mojo_per_tranzact))
-    elif height < 7 * _blocks_per_year:
-        return uint64(int((1 / 8) * 1.5 * _mojo_per_tranzact))
+    #no premine, but early adopters get paid!
+    if height < _blocks_per_year / 12:
+        return uint64(int((1 / 8) * 15 * _zacto_per_tranzact))
+    elif height < 1 * _blocks_per_year:
+        return uint64(int((1 / 8) * 5 * _zacto_per_tranzact))
+    elif height < 3 * _blocks_per_year:
+        return uint64(int((1 / 8) * 2 * _zacto_per_tranzact))
+    elif height < 6 * _blocks_per_year:
+        return uint64(int((1 / 8) * 1 * _zacto_per_tranzact))
     elif height < 12 * _blocks_per_year:
-        return uint64(int((1 / 8) * 0.75 * _mojo_per_tranzact))
+        return uint64(int((1 / 8) * 0.5 * _zacto_per_tranzact))
     else:
-        return uint64(int((1 / 8) * 0.25 * _mojo_per_tranzact))
+        return uint64(int((1 / 8) * 0.25 * _zacto_per_tranzact))
