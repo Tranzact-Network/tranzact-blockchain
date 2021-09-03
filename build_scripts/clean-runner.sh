Chia-Network/chia-blockchain

Clean up a few more potential leftover files in chia-blockchain-gui (#8230)
* Clean up a few more potential leftover files in chia-blockchain-gui

* Make shellcheck happy

build_scripts/clean-runner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Cleans up files/directories that may be left over from previous runs for a clean slate before starting a new build
 
+PWD=$(pwd)
+
 rm -rf ../venv || true
 rm -rf venv || true
 rm -rf chia_blockchain.egg-info || true
@@ -10,6 +12,9 @@ rm -rf build_scripts/pyinstaller || true
 rm -rf chia-blockchain-gui/build || true
 rm -rf chia-blockchain-gui/daemon || true
 rm -rf chia-blockchain-gui/node_modules || true
+rm chia-blockchain-gui/temp.json || true
+( cd "$PWD/chia-blockchain-gui" && git checkout HEAD -- package-lock.json ) || true
+cd "$PWD" || true
 
 # Do our best to get rid of any globally installed notarize-cli versions so the version in the current build script is
 # installed without conflicting with the other version that might be installed

Tranzact-Network/tranzact-blockchain

build_scripts/clean-runner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Cleans up files/directories that may be left over from previous runs for a clean slate before starting a new build
 
+PWD=$(pwd)
+
 rm -rf ../venv || true
 rm -rf venv || true
 rm -rf tranzact_blockchain.egg-info || true
@@ -10,6 +12,9 @@ rm -rf build_scripts/pyinstaller || true
 rm -rf tranzact-blockchain-gui/build || true
 rm -rf tranzact-blockchain-gui/daemon || true
 rm -rf tranzact-blockchain-gui/node_modules || true
+rm tranzact-blockchain-gui/temp.json || true
+( cd "$PWD/tranzact-blockchain-gui" && git checkout HEAD -- package-lock.json ) || true
+cd "$PWD" || true
 
 # Do our best to get rid of any globally installed notarize-cli versions so the version in the current build script is
 # installed without conflicting with the other version that might be installed