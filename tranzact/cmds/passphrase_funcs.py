Chia-Network/chia-blockchain

macOS keyring.yaml support (#8292)
* Added 'service' as a Keychain ctor param. Removed 'testing'

* Detect existing keys in the Mac Keychain

* Fix to allow migration of keys on macOS

* Added dump_keyring.py tool to show decrypted contents of keyring.yaml

* Prompt to save passphrase to macOS keychain

* Master passphrase retrieval/removal from the macOS Keychain.
Fixed typos.

* Warn if errSecInteractionNotAllowed is detected when accessing the macOS Keychain

* Fixed file_keyring synchronization test failures on macOS.
Fixed sporadic test failures on macOS when fsevents are delivered for the keyring after deletion.
TempKeyring-based tests now patch supports_os_passphrase_storage() to return False.

* TempKeyring mocks-out legacy_keyring setup to allow tests to succeed on macOS (which could find existing keys in the Keychain)

* Fixed pylint error

* Use with_name instead of with_stem (which is new to Python 3.9)

* Fixed keychain tests that started prompting for the keyring passphrase.

* Fixed LGTM issues

* Re-added the cleaning up temp keychain statement. This is being removed in a separate PR.

* Linter fixes

* Fixed keyring assignment on macOS when passphrase support is disabled.

* Include 'can_save_passphrase' flag in keyring_status response

* More linter fixes

* Fixed determination of the user_passphrase_is_set flag. This was returning true for a newly created keyring without any keys (or passphrase set)

* Removed the tidy_passphrase function per feedback

* Added some comments based on feedback

* Update chia/cmds/passphrase_funcs.py

Co-authored-by: Adam Kelly <338792+aqk@users.noreply.github.com>

Co-authored-by: Adam Kelly <338792+aqk@users.noreply.github.com>

chia/cmds/passphrase_funcs.py
@@ -2,8 +2,9 @@ import click
 import sys
 
 from chia.daemon.client import acquire_connection_to_daemon
-from chia.util.keychain import Keychain, obtain_current_passphrase
+from chia.util.keychain import Keychain, obtain_current_passphrase, supports_os_passphrase_storage
 from chia.util.keyring_wrapper import DEFAULT_PASSPHRASE_IF_NO_MASTER_PASSPHRASE
+from chia.util.misc import prompt_yes_no
 from chia.util.ws_message import WsRpcMessage
 from getpass import getpass
 from io import TextIOWrapper
@@ -42,33 +43,56 @@ def verify_passphrase_meets_requirements(
         raise Exception("Unexpected passphrase verification case")
 
 
-def tidy_passphrase(passphrase: str) -> str:
-    """
-    Perform any string processing we want to apply to the entered passphrase.
-    Currently we strip leading/trailing whitespace.
-    """
-    return passphrase.strip()
+def prompt_to_save_passphrase() -> bool:
+    save: bool = False
+
+    try:
+        if supports_os_passphrase_storage():
+            location: Optional[str] = None
+
+            if sys.platform == "darwin":
+                location = "macOS Keychain"
+
+            if location is None:
+                raise ValueError("OS-specific credential store not specified")
+
+            print(
+                "\n"
+                "Your passphrase can be stored in your system's secure credential store. "
+                "Other Chia processes will be able to access your keys without prompting for your passphrase."
+            )
+            save = prompt_yes_no(f"Would you like to save your passphrase to the {location} (y/n) ")
+
+    except Exception as e:
+        print(f"Caught exception: {e}")
+        return False
+
+    return save
 
 
-def prompt_for_new_passphrase() -> str:
+def prompt_for_new_passphrase() -> Tuple[str, bool]:
     min_length: int = Keychain.minimum_passphrase_length()
     if min_length > 0:
         n = min_length
         print(f"\nPassphrases must be {n} or more characters in length")  # lgtm [py/clear-text-logging-sensitive-data]
     while True:
-        passphrase = tidy_passphrase(getpass("New Passphrase: "))
-        confirmation = tidy_passphrase(getpass("Confirm Passphrase: "))
+        passphrase: str = getpass("New Passphrase: ")
+        confirmation: str = getpass("Confirm Passphrase: ")
+        save_passphrase: bool = False
 
         valid_passphrase, error_msg = verify_passphrase_meets_requirements(passphrase, confirmation)
 
         if valid_passphrase:
-            return passphrase
+            if supports_os_passphrase_storage():
+                save_passphrase = prompt_to_save_passphrase()
+
+            return passphrase, save_passphrase
         elif error_msg:
             print(f"{error_msg}\n")  # lgtm [py/clear-text-logging-sensitive-data]
 
 
 def read_passphrase_from_file(passphrase_file: TextIOWrapper) -> str:
-    passphrase = tidy_passphrase(passphrase_file.read())
+    passphrase = passphrase_file.read()
     passphrase_file.close()
     return passphrase
 
@@ -82,14 +106,18 @@ def initialize_passphrase() -> None:
     # We'll rely on Keyring initialization to leverage the cached passphrase for
     # bootstrapping the keyring encryption process
     print("Setting keyring passphrase")
-    passphrase = None
+    passphrase: Optional[str] = None
+    # save_passphrase indicates whether the passphrase should be saved in the
+    # macOS Keychain or Windows Credential Manager
+    save_passphrase: bool = False
+
     if Keychain.has_cached_passphrase():
         passphrase = Keychain.get_cached_master_passphrase()
 
     if not passphrase or passphrase == default_passphrase():
-        passphrase = prompt_for_new_passphrase()
+        passphrase, save_passphrase = prompt_for_new_passphrase()
 
-    Keychain.set_master_passphrase(current_passphrase=None, new_passphrase=passphrase)
+    Keychain.set_master_passphrase(current_passphrase=None, new_passphrase=passphrase, save_passphrase=save_passphrase)
 
 
 def set_or_update_passphrase(passphrase: Optional[str], current_passphrase: Optional[str]) -> bool:
@@ -106,17 +134,21 @@ def set_or_update_passphrase(passphrase: Optional[str], current_passphrase: Opti
                 print(f"Unable to confirm current passphrase: {e}")
                 sys.exit(1)
 
-    success = False
-    new_passphrase = passphrase
+    success: bool = False
+    new_passphrase: Optional[str] = passphrase
+    save_passphrase: bool = False
+
     try:
         # Prompt for the new passphrase, if necessary
-        if not new_passphrase:
-            new_passphrase = prompt_for_new_passphrase()
+        if new_passphrase is None:
+            new_passphrase, save_passphrase = prompt_for_new_passphrase()
 
         if new_passphrase == current_passphrase:
             raise ValueError("passphrase is unchanged")
 
-        Keychain.set_master_passphrase(current_passphrase=current_passphrase, new_passphrase=new_passphrase)
+        Keychain.set_master_passphrase(
+            current_passphrase=current_passphrase, new_passphrase=new_passphrase, save_passphrase=save_passphrase
+        )
         success = True
     except Exception as e:
         print(f"Unable to set or update passphrase: {e}")

Tranzact-Network/tranzact-blockchain

tranzact/cmds/passphrase_funcs.py
@@ -1,7 +1,10 @@
 import sys
 
-from tranzact.util.keychain import Keychain, obtain_current_passphrase
+from tranzact.daemon.client import acquire_connection_to_daemon
+from tranzact.util.keychain import Keychain, obtain_current_passphrase, supports_os_passphrase_storage
 from tranzact.util.keyring_wrapper import DEFAULT_PASSPHRASE_IF_NO_MASTER_PASSPHRASE
+from tranzact.util.misc import prompt_yes_no
+from tranzact.util.ws_message import WsRpcMessage
 from getpass import getpass
 from io import TextIOWrapper
 from pathlib import Path
@@ -39,32 +42,56 @@ def verify_passphrase_meets_requirements(
         raise Exception("Unexpected passphrase verification case")
 
 
-def tidy_passphrase(passphrase: str) -> str:
-    """
-    Perform any string processing we want to apply to the entered passphrase.
-    Currently we strip leading/trailing whitespace.
-    """
-    return passphrase.strip()
+def prompt_to_save_passphrase() -> bool:
+    save: bool = False
+
+    try:
+        if supports_os_passphrase_storage():
+            location: Optional[str] = None
+
+            if sys.platform == "darwin":
+                location = "macOS Keychain"
+
+            if location is None:
+                raise ValueError("OS-specific credential store not specified")
+
+            print(
+                "\n"
+                "Your passphrase can be stored in your system's secure credential store. "
+                "Other Chia processes will be able to access your keys without prompting for your passphrase."
+            )
+            save = prompt_yes_no(f"Would you like to save your passphrase to the {location} (y/n) ")
+
+    except Exception as e:
+        print(f"Caught exception: {e}")
+        return False
+
+    return save
 
 
-def prompt_for_new_passphrase() -> str:
-    if MIN_PASSPHRASE_LEN > 0:
-        n = MIN_PASSPHRASE_LEN
+def prompt_for_new_passphrase() -> Tuple[str, bool]:
+    min_length: int = Keychain.minimum_passphrase_length()
+    if min_length > 0:
+        n = min_length
         print(f"\nPassphrases must be {n} or more characters in length")  # lgtm [py/clear-text-logging-sensitive-data]
     while True:
-        passphrase = tidy_passphrase(getpass("New Passphrase: "))
-        confirmation = tidy_passphrase(getpass("Confirm Passphrase: "))
+        passphrase: str = getpass("New Passphrase: ")
+        confirmation: str = getpass("Confirm Passphrase: ")
+        save_passphrase: bool = False
 
         valid_passphrase, error_msg = verify_passphrase_meets_requirements(passphrase, confirmation)
 
         if valid_passphrase:
-            return passphrase
+            if supports_os_passphrase_storage():
+                save_passphrase = prompt_to_save_passphrase()
+
+            return passphrase, save_passphrase
         elif error_msg:
             print(f"{error_msg}\n")  # lgtm [py/clear-text-logging-sensitive-data]
 
 
 def read_passphrase_from_file(passphrase_file: TextIOWrapper) -> str:
-    passphrase = tidy_passphrase(passphrase_file.read())
+    passphrase = passphrase_file.read()
     passphrase_file.close()
     return passphrase
 
@@ -78,14 +105,18 @@ def initialize_passphrase() -> None:
     # We'll rely on Keyring initialization to leverage the cached passphrase for
     # bootstrapping the keyring encryption process
     print("Setting keyring passphrase")
-    passphrase = None
+    passphrase: Optional[str] = None
+    # save_passphrase indicates whether the passphrase should be saved in the
+    # macOS Keychain or Windows Credential Manager
+    save_passphrase: bool = False
+
     if Keychain.has_cached_passphrase():
         passphrase = Keychain.get_cached_master_passphrase()
 
     if not passphrase or passphrase == default_passphrase():
-        passphrase = prompt_for_new_passphrase()
+        passphrase, save_passphrase = prompt_for_new_passphrase()
 
-    Keychain.set_master_passphrase(current_passphrase=None, new_passphrase=passphrase)
+    Keychain.set_master_passphrase(current_passphrase=None, new_passphrase=passphrase, save_passphrase=save_passphrase)
 
 
 def set_or_update_passphrase(passphrase: Optional[str], current_passphrase: Optional[str]) -> bool:
@@ -102,17 +133,21 @@ def set_or_update_passphrase(passphrase: Optional[str], current_passphrase: Opti
                 print(f"Unable to confirm current passphrase: {e}")
                 sys.exit(1)
 
-    success = False
-    new_passphrase = passphrase
+    success: bool = False
+    new_passphrase: Optional[str] = passphrase
+    save_passphrase: bool = False
+
     try:
         # Prompt for the new passphrase, if necessary
-        if not new_passphrase:
-            new_passphrase = prompt_for_new_passphrase()
+        if new_passphrase is None:
+            new_passphrase, save_passphrase = prompt_for_new_passphrase()
 
         if new_passphrase == current_passphrase:
             raise ValueError("passphrase is unchanged")
 
-        Keychain.set_master_passphrase(current_passphrase=current_passphrase, new_passphrase=new_passphrase)
+        Keychain.set_master_passphrase(
+            current_passphrase=current_passphrase, new_passphrase=new_passphrase, save_passphrase=save_passphrase
+        )
         success = True
     except Exception as e:
         print(f"Unable to set or update passphrase: {e}")