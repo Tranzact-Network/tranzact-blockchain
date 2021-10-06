Chia-Network/chia-blockchain

Fullnode related changes from wallet protocol and unharded derivation (#8522)
* fullnode related changes from wallet protocol and unharded derivation

* limit total subscriptions per peer

* reset counter on disconnect

* dict not a set

* check membership

* remove unused tests, lint

tests/connection_utils.py
@@ -28,7 +28,9 @@ async def disconnect_all_and_reconnect(server: ChiaServer, reconnect_to: ChiaSer
     return await server.start_client(PeerInfo(self_hostname, uint16(reconnect_to._port)), None)
 
 
-async def add_dummy_connection(server: ChiaServer, dummy_port: int) -> Tuple[asyncio.Queue, bytes32]:
+async def add_dummy_connection(
+    server: ChiaServer, dummy_port: int, type: NodeType = NodeType.FULL_NODE
+) -> Tuple[asyncio.Queue, bytes32]:
     timeout = aiohttp.ClientTimeout(total=10)
     session = aiohttp.ClientSession(timeout=timeout)
     incoming_queue: asyncio.Queue = asyncio.Queue()
@@ -46,7 +48,7 @@ async def add_dummy_connection(server: ChiaServer, dummy_port: int) -> Tuple[asy
     url = f"wss://{self_hostname}:{server._port}/ws"
     ws = await session.ws_connect(url, autoclose=True, autoping=True, ssl=ssl_context)
     wsc = WSChiaConnection(
-        NodeType.FULL_NODE,
+        type,
         ws,
         server._port,
         log,

chia/full_node/full_node_api.py
@@ -18,7 +18,7 @@ from chia.full_node.signage_point import SignagePoint
 from chia.protocols import farmer_protocol, full_node_protocol, introducer_protocol, timelord_protocol, wallet_protocol
 from chia.protocols.full_node_protocol import RejectBlock, RejectBlocks
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
-from chia.protocols.wallet_protocol import PuzzleSolutionResponse, RejectHeaderBlocks, RejectHeaderRequest
+from chia.protocols.wallet_protocol import PuzzleSolutionResponse, RejectHeaderBlocks, RejectHeaderRequest, CoinState
 from chia.server.outbound_message import Message, make_msg
 from chia.types.blockchain_format.coin import Coin, hash_coin_list
 from chia.types.blockchain_format.pool_target import PoolTarget
@@ -1311,3 +1311,75 @@ class FullNodeAPI:
         if self.full_node.sync_store.get_sync_mode():
             return None
         await self.full_node.respond_compact_vdf(request, peer)
+
+    @peer_required
+    @api_request
+    async def register_interest_in_puzzle_hash(
+        self, request: wallet_protocol.RegisterForPhUpdates, peer: ws.WSChiaConnection
+    ):
+        if peer.peer_node_id not in self.full_node.peer_puzzle_hash:
+            self.full_node.peer_puzzle_hash[peer.peer_node_id] = set()
+
+        if peer.peer_node_id not in self.full_node.peer_sub_counter:
+            self.full_node.peer_sub_counter[peer.peer_node_id] = 0
+
+        # Add peer to the "Subscribed" dictionary
+        for puzzle_hash in request.puzzle_hashes:
+            if puzzle_hash not in self.full_node.ph_subscriptions:
+                self.full_node.ph_subscriptions[puzzle_hash] = set()
+            if (
+                peer.peer_node_id not in self.full_node.ph_subscriptions[puzzle_hash]
+                and self.full_node.peer_sub_counter[peer.peer_node_id] < 100000
+            ):
+                self.full_node.ph_subscriptions[puzzle_hash].add(peer.peer_node_id)
+                self.full_node.peer_puzzle_hash[peer.peer_node_id].add(puzzle_hash)
+                self.full_node.peer_sub_counter[peer.peer_node_id] += 1
+
+        # Send all coins with requested puzzle hash that have been created after the specified height
+        states: List[CoinState] = await self.full_node.coin_store.get_coin_states_by_puzzle_hashes(
+            include_spent_coins=True, puzzle_hashes=request.puzzle_hashes, start_height=request.min_height
+        )
+
+        response = wallet_protocol.RespondToPhUpdates(request.puzzle_hashes, request.min_height, states)
+        msg = make_msg(ProtocolMessageTypes.respond_to_ph_update, response)
+        return msg
+
+    @peer_required
+    @api_request
+    async def register_interest_in_coin(
+        self, request: wallet_protocol.RegisterForCoinUpdates, peer: ws.WSChiaConnection
+    ):
+        if peer.peer_node_id not in self.full_node.peer_coin_ids:
+            self.full_node.peer_coin_ids[peer.peer_node_id] = set()
+
+        if peer.peer_node_id not in self.full_node.peer_sub_counter:
+            self.full_node.peer_sub_counter[peer.peer_node_id] = 0
+
+        for coin_id in request.coin_ids:
+            if coin_id not in self.full_node.coin_subscriptions:
+                self.full_node.coin_subscriptions[coin_id] = set()
+            if (
+                peer.peer_node_id not in self.full_node.coin_subscriptions[coin_id]
+                and self.full_node.peer_sub_counter[peer.peer_node_id] < 100000
+            ):
+                self.full_node.coin_subscriptions[coin_id].add(peer.peer_node_id)
+                self.full_node.peer_coin_ids[peer.peer_node_id].add(coin_id)
+                self.full_node.peer_sub_counter[peer.peer_node_id] += 1
+
+        states: List[CoinState] = await self.full_node.coin_store.get_coin_state_by_ids(
+            include_spent_coins=True, coin_ids=request.coin_ids, start_height=request.min_height
+        )
+
+        response = wallet_protocol.RespondToCoinUpdates(request.coin_ids, request.min_height, states)
+        msg = make_msg(ProtocolMessageTypes.respond_to_coin_update, response)
+        return msg
+
+    @api_request
+    async def request_children(self, request: wallet_protocol.RequestChildren) -> Optional[Message]:
+        coin_records: List[CoinRecord] = await self.full_node.coin_store.get_coin_records_by_parent_ids(
+            True, [request.coin_name]
+        )
+        states = [record.coin_state for record in coin_records]
+        response = wallet_protocol.RespondChildren(states)
+        msg = make_msg(ProtocolMessageTypes.respond_children, response)
+        return msg

chia/types/coin_record.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from typing import Optional
 
+from chia.protocols.wallet_protocol import CoinState
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.ints import uint32, uint64
@@ -24,3 +26,13 @@ class CoinRecord(Streamable):
     @property
     def name(self) -> bytes32:
         return self.coin.name()
+
+    @property
+    def coin_state(self) -> CoinState:
+        spent_h = None
+        if self.spent:
+            spent_h = self.spent_block_index
+        confirmed_height: Optional[uint32] = self.confirmed_block_index
+        if self.confirmed_block_index == 0 and self.timestamp == 0:
+            confirmed_height = None
+        return CoinState(self.coin, spent_h, confirmed_height)

chia/types/coin_spend.py
@@ -3,7 +3,7 @@ from typing import List
 
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import SerializedProgram, INFINITE_COST
-from chia.util.chain_utils import additions_for_solution
+from chia.util.chain_utils import additions_for_solution, fee_for_solution
 from chia.util.streamable import Streamable, streamable
 
 
@@ -22,3 +22,6 @@ class CoinSpend(Streamable):
 
     def additions(self) -> List[Coin]:
         return additions_for_solution(self.coin.name(), self.puzzle_reveal, self.solution, INFINITE_COST)
+
+    def reserved_fee(self) -> int:
+        return fee_for_solution(self.puzzle_reveal, self.solution, INFINITE_COST)

chia/util/chain_utils.py
@@ -1,8 +1,11 @@
 from typing import List
 
+from clvm.casts import int_from_bytes
+
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import SerializedProgram
 from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.types.condition_opcodes import ConditionOpcode
 from chia.util.condition_tools import (
     conditions_dict_for_solution,
     created_outputs_for_conditions_dict,
@@ -19,3 +22,16 @@ def additions_for_solution(
     if err or dic is None:
         return []
     return created_outputs_for_conditions_dict(dic, coin_name)
+
+
+def fee_for_solution(puzzle_reveal: SerializedProgram, solution: SerializedProgram, max_cost: int) -> int:
+    err, dic, cost = conditions_dict_for_solution(puzzle_reveal, solution, max_cost)
+    if err or dic is None:
+        return 0
+
+    total = 0
+    for cvp in dic.get(ConditionOpcode.RESERVE_FEE, []):
+        amount_bin = cvp.vars[0]
+        amount = int_from_bytes(amount_bin)
+        total += amount
+    return total

Tranzact-Network/tranzact-blockchain

tests/connection_utils.py
@@ -28,7 +28,9 @@ async def disconnect_all_and_reconnect(server: TranzactServer, reconnect_to: Tra
     return await server.start_client(PeerInfo(self_hostname, uint16(reconnect_to._port)), None)
 
 
-async def add_dummy_connection(server: TranzactServer, dummy_port: int) -> Tuple[asyncio.Queue, bytes32]:
+async def add_dummy_connection(
+    server: TranzactServer, dummy_port: int, type: NodeType = NodeType.FULL_NODE
+) -> Tuple[asyncio.Queue, bytes32]:
     timeout = aiohttp.ClientTimeout(total=10)
     session = aiohttp.ClientSession(timeout=timeout)
     incoming_queue: asyncio.Queue = asyncio.Queue()
@@ -46,7 +48,7 @@ async def add_dummy_connection(server: TranzactServer, dummy_port: int) -> Tuple
     url = f"wss://{self_hostname}:{server._port}/ws"
     ws = await session.ws_connect(url, autoclose=True, autoping=True, ssl=ssl_context)
     wsc = WSTranzactConnection(
-        NodeType.FULL_NODE,
+        type,
         ws,
         server._port,
         log,

tranzact/full_node/full_node_api.py
@@ -18,7 +18,7 @@ from tranzact.full_node.signage_point import SignagePoint
 from tranzact.protocols import farmer_protocol, full_node_protocol, introducer_protocol, timelord_protocol, wallet_protocol
 from tranzact.protocols.full_node_protocol import RejectBlock, RejectBlocks
 from tranzact.protocols.protocol_message_types import ProtocolMessageTypes
-from tranzact.protocols.wallet_protocol import PuzzleSolutionResponse, RejectHeaderBlocks, RejectHeaderRequest
+from tranzact.protocols.wallet_protocol import PuzzleSolutionResponse, RejectHeaderBlocks, RejectHeaderRequest, CoinState
 from tranzact.server.outbound_message import Message, make_msg
 from tranzact.types.blockchain_format.coin import Coin, hash_coin_list
 from tranzact.types.blockchain_format.pool_target import PoolTarget
@@ -1311,3 +1311,75 @@ class FullNodeAPI:
         if self.full_node.sync_store.get_sync_mode():
             return None
         await self.full_node.respond_compact_vdf(request, peer)
+
+    @peer_required
+    @api_request
+    async def register_interest_in_puzzle_hash(
+        self, request: wallet_protocol.RegisterForPhUpdates, peer: ws.WSTranzactConnection
+    ):
+        if peer.peer_node_id not in self.full_node.peer_puzzle_hash:
+            self.full_node.peer_puzzle_hash[peer.peer_node_id] = set()
+
+        if peer.peer_node_id not in self.full_node.peer_sub_counter:
+            self.full_node.peer_sub_counter[peer.peer_node_id] = 0
+
+        # Add peer to the "Subscribed" dictionary
+        for puzzle_hash in request.puzzle_hashes:
+            if puzzle_hash not in self.full_node.ph_subscriptions:
+                self.full_node.ph_subscriptions[puzzle_hash] = set()
+            if (
+                peer.peer_node_id not in self.full_node.ph_subscriptions[puzzle_hash]
+                and self.full_node.peer_sub_counter[peer.peer_node_id] < 100000
+            ):
+                self.full_node.ph_subscriptions[puzzle_hash].add(peer.peer_node_id)
+                self.full_node.peer_puzzle_hash[peer.peer_node_id].add(puzzle_hash)
+                self.full_node.peer_sub_counter[peer.peer_node_id] += 1
+
+        # Send all coins with requested puzzle hash that have been created after the specified height
+        states: List[CoinState] = await self.full_node.coin_store.get_coin_states_by_puzzle_hashes(
+            include_spent_coins=True, puzzle_hashes=request.puzzle_hashes, start_height=request.min_height
+        )
+
+        response = wallet_protocol.RespondToPhUpdates(request.puzzle_hashes, request.min_height, states)
+        msg = make_msg(ProtocolMessageTypes.respond_to_ph_update, response)
+        return msg
+
+    @peer_required
+    @api_request
+    async def register_interest_in_coin(
+        self, request: wallet_protocol.RegisterForCoinUpdates, peer: ws.WSTranzactConnection
+    ):
+        if peer.peer_node_id not in self.full_node.peer_coin_ids:
+            self.full_node.peer_coin_ids[peer.peer_node_id] = set()
+
+        if peer.peer_node_id not in self.full_node.peer_sub_counter:
+            self.full_node.peer_sub_counter[peer.peer_node_id] = 0
+
+        for coin_id in request.coin_ids:
+            if coin_id not in self.full_node.coin_subscriptions:
+                self.full_node.coin_subscriptions[coin_id] = set()
+            if (
+                peer.peer_node_id not in self.full_node.coin_subscriptions[coin_id]
+                and self.full_node.peer_sub_counter[peer.peer_node_id] < 100000
+            ):
+                self.full_node.coin_subscriptions[coin_id].add(peer.peer_node_id)
+                self.full_node.peer_coin_ids[peer.peer_node_id].add(coin_id)
+                self.full_node.peer_sub_counter[peer.peer_node_id] += 1
+
+        states: List[CoinState] = await self.full_node.coin_store.get_coin_state_by_ids(
+            include_spent_coins=True, coin_ids=request.coin_ids, start_height=request.min_height
+        )
+
+        response = wallet_protocol.RespondToCoinUpdates(request.coin_ids, request.min_height, states)
+        msg = make_msg(ProtocolMessageTypes.respond_to_coin_update, response)
+        return msg
+
+    @api_request
+    async def request_children(self, request: wallet_protocol.RequestChildren) -> Optional[Message]:
+        coin_records: List[CoinRecord] = await self.full_node.coin_store.get_coin_records_by_parent_ids(
+            True, [request.coin_name]
+        )
+        states = [record.coin_state for record in coin_records]
+        response = wallet_protocol.RespondChildren(states)
+        msg = make_msg(ProtocolMessageTypes.respond_children, response)
+        return msg

tranzact/types/coin_record.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from typing import Optional
 
+from tranzact.protocols.wallet_protocol import CoinState
 from tranzact.types.blockchain_format.coin import Coin
 from tranzact.types.blockchain_format.sized_bytes import bytes32
 from tranzact.util.ints import uint32, uint64
@@ -24,3 +26,13 @@ class CoinRecord(Streamable):
     @property
     def name(self) -> bytes32:
         return self.coin.name()
+
+    @property
+    def coin_state(self) -> CoinState:
+        spent_h = None
+        if self.spent:
+            spent_h = self.spent_block_index
+        confirmed_height: Optional[uint32] = self.confirmed_block_index
+        if self.confirmed_block_index == 0 and self.timestamp == 0:
+            confirmed_height = None
+        return CoinState(self.coin, spent_h, confirmed_height)

tranzact/types/coin_spend.py
@@ -3,7 +3,7 @@ from typing import List
 
 from tranzact.types.blockchain_format.coin import Coin
 from tranzact.types.blockchain_format.program import SerializedProgram, INFINITE_COST
-from tranzact.util.chain_utils import additions_for_solution
+from tranzact.util.chain_utils import additions_for_solution, fee_for_solution
 from tranzact.util.streamable import Streamable, streamable
 
 
@@ -22,3 +22,6 @@ class CoinSpend(Streamable):
 
     def additions(self) -> List[Coin]:
         return additions_for_solution(self.coin.name(), self.puzzle_reveal, self.solution, INFINITE_COST)
+
+    def reserved_fee(self) -> int:
+        return fee_for_solution(self.puzzle_reveal, self.solution, INFINITE_COST)

tranzact/util/chain_utils.py
@@ -1,8 +1,11 @@
 from typing import List
 
+from clvm.casts import int_from_bytes
+
 from tranzact.types.blockchain_format.coin import Coin
 from tranzact.types.blockchain_format.program import SerializedProgram
 from tranzact.types.blockchain_format.sized_bytes import bytes32
+from tranzact.types.condition_opcodes import ConditionOpcode
 from tranzact.util.condition_tools import (
     conditions_dict_for_solution,
     created_outputs_for_conditions_dict,
@@ -19,3 +22,16 @@ def additions_for_solution(
     if err or dic is None:
         return []
     return created_outputs_for_conditions_dict(dic, coin_name)
+
+
+def fee_for_solution(puzzle_reveal: SerializedProgram, solution: SerializedProgram, max_cost: int) -> int:
+    err, dic, cost = conditions_dict_for_solution(puzzle_reveal, solution, max_cost)
+    if err or dic is None:
+        return 0
+
+    total = 0
+    for cvp in dic.get(ConditionOpcode.RESERVE_FEE, []):
+        amount_bin = cvp.vars[0]
+        amount = int_from_bytes(amount_bin)
+        total += amount
+    return total